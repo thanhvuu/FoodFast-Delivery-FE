12143a17b1eee6469bb0e4077abc5343b0a87a63
khanhcrs <khanhtrantuan12a621@gmail.com>
2025-10-01 16:17:39 +0700

    Do An

---
+++ src/assets/assets.js
@@ -0,0 +1,12 @@
+import logo from './logo.jpg';
+import search_icon from './search_icon.png';
+import basket_icon from './basket.jpeg';
+import header_img from './header_img.webp';
+
+
+export const assets = {
+    logo,
+    search_icon,
+    basket_icon,
+    header_img,
+}
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order your favorite food here</h2>
+                <p>Healthy switcher chefs do all the prep work, like chopping and marinating, so you can cook a fresh, delicious meal in just 15 minutes.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
