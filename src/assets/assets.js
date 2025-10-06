2e2c21e19dda744fcc15c9af723404a5590f0e0d
khanhcrs <khanhtrantuan12a621@gmail.com>
2025-10-06 18:57:24 +0700

    update them san pham

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,6 +2,16 @@ import logo from './logo.jpg';
 import search_icon from './search_icon.png';
 import basket_icon from './basket.jpeg';
 import header_img from './header_img.webp';
+import menu_1 from './menu1.jpg';
+import menu_2 from './menu2.jpg';
+import menu_3 from './menu3.jpg';
+import menu_4 from './menu4.jpg';
+import menu_5 from './menu5.jpg';
+import menu_6 from './menu6.jpeg';
+
+import food_1 from './food_1.jpg';
+import food_2 from './food_2.jpg';
+import food_3 from './food_3.webp';
 
 
 export const assets = {
@@ -10,3 +20,57 @@ export const assets = {
     basket_icon,
     header_img,
 }
+
+export const menu_list = [
+    {
+        menu_name: "fast-food",
+        menu_image: menu_1
+    },
+    {
+        menu_name: "Đồ ăn vặt",
+        menu_image: menu_2
+    },
+    {
+        menu_name: "Đồ ngọt",
+        menu_image: menu_3
+    },
+    {
+        menu_name: "Món nước",
+        menu_image: menu_4
+    },
+    {
+        menu_name: "Món khô",
+        menu_image: menu_6
+    },
+    {
+        menu_name: "Thức Uống",
+        menu_image: menu_5
+    },
+]
+
+export const food_list = [
+    {
+        _id: "1",
+        name: "Gà rán",
+        image: food_1,
+        price: 30000,
+        description: "Gà rán giòn rụm, thơm ngon",
+        category: "fast-food"
+    },
+    {
+        _id: "2",
+        name: "Hamburger",
+        image: food_2,
+        price: 35000,
+        description: "Hamburger bò Mỹ, phô mai cheddar",
+        category: "fast-food",
+    },
+    {
+        _id: "3",
+        name: "Pizza",
+        image: food_3,
+        price: 50000,
+        description: "Pizza hải sản, phô mai mozzarella",
+        category: "fast-food",
+    },
+]
--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
     return (
-        <div>
-
+        <div className='explore-menu' id='explore-menu'>
+            <h1>Khám phá menu của chúng tôi</h1>
+            <p className='explore-menu-text'>Chọn những món ăn hợp khẩu vị của mình 1 cách dễ dàng khi đặt qua app</p>
+            <div className="explore-menu-list">
+                {menu_list.map((item, index) => {
+                    return (
+                        <div onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)} key={index} className="explore-menu-list-item">
+                            <img className={category === item.menu_name ? "active" : ""} src={item.menu_image} alt="" />
+                            <p>{item.menu_name}</p>
+                        </div>
+                    )
+                })}
+            </div>
+            <hr />
         </div>
     )
 }
